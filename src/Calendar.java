7250dc3f3244cb7783b5290cd79bf1cf16f9d502
EmilDagsberg <cph-ed113@cphbusiness.dk>
2024-12-09 11:45:23 +0100

    viser en calender

--- src/Calendar.java
+++ src/Calendar.java
@@ -20,9 +20,28 @@ public class Calendar {
         ui.displayMsg("\n   " + cal.getDisplayName(GregorianCalendar.MONTH, GregorianCalendar.LONG, Locale.ENGLISH) + " " + year);
         ui.displayMsg("Su Mo Tu We Th Fr Sa");
 
-        int fir
+        int firstDayOfWeek = cal.get(GregorianCalendar.DAY_OF_WEEK);
 
+        // Get the number of days in the month
+        int daysInMonth = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
+
+        // Print spaces for the days before the first day of the month
+        for (int i = 1; i < firstDayOfWeek; i++) {
+            ui.displayMsg("   ");
+        }
+
+        // Print the days of the month
+        for (int day = 1; day <= daysInMonth; day++) {
+            System.out.printf("%2d ", day);
+
+            // Start a new line after Saturday
+            if ((day + firstDayOfWeek - 1) % 7 == 0) {
+                ui.displayMsg("");
+            }
+        }
+        ui.displayMsg(""); // Add a new line at the end
     }
+
 
     public void addToCalendar(){
 
