55ef120eabdda6d8f6b08af7f9c4e816d6ebfb00
Bojuuh <mbjerrumj@gmail.com>
2024-12-09 11:32:53 +0100

    WorkoutProgram v0.1 Menu changed accordingly

--- src/Menu.java
+++ src/Menu.java
@@ -1,12 +1,14 @@
 public class Menu {
     private User currentUser;
     private TextUI ui;
+    private WorkoutProgram workoutProgram;
+
     public Menu(User currentUser) {
         this.currentUser = currentUser;
         ui = new TextUI();
     }
 
-    void displayMenu() {
+    public void displayMenu() {
         ui.displayMsg("        Main Menu        \n========================= \n1. Workout \n2. Workout Program \n3. Calendar \n4. Stats\n5. Settings");
         int choice = ui.promptNumeric("Enter number of menu:");
 
@@ -14,6 +16,8 @@ public class Menu {
             System.out.println("Entering workout tab...");
         } else if (choice == 2){
             System.out.println("Entering Workout program tab...");
+            this.workoutProgram = new WorkoutProgram(currentUser);
+            workoutProgram.displayWorkoutProgramMenu();
         } else if (choice == 3){
             System.out.println("Entering Calendar...");
         } else if (choice == 4){
