73e124f858113866196ddfbcf425877103b72e68
Bojuuh <mbjerrumj@gmail.com>
2024-12-09 10:45:59 +0100

    Menu Class contents v0.1

--- src/Menu.java
+++ src/Menu.java
@@ -1,11 +1,27 @@
 public class Menu {
     private User currentUser;
-
+    private TextUI ui;
     public Menu(User currentUser) {
         this.currentUser = currentUser;
+        ui = new TextUI();
     }
 
     void displayMenu() {
-        System.out.println("Menu");
+        ui.displayMsg("        Main Menu        \n========================= \n1. Workout \n2. Workout Program \n3. Calendar \n4. Stats\n5. Settings");
+        int choice = ui.promptNumeric("Enter number of menu:");
+
+        if (choice == 1){
+            System.out.println("Entering workout tab...");
+        } else if (choice == 2){
+            System.out.println("Entering Workout program tab...");
+        } else if (choice == 3){
+            System.out.println("Entering Calendar...");
+        } else if (choice == 4){
+            System.out.println("Entering stats page...");
+        } else if (choice == 5){
+            System.out.println("Entering settings...");
+        }
+
+
     }
 }
